5f7eeada6661bab527d66966b16d854d68804ac8
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-17 20:38:39 -0300

    create new folders and update server and interfaces

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import fastify, { FastifyInstance } from "fastify";
+import { userRoutes } from './routes/user.routes';
 
 const app: FastifyInstance = fastify();
+
+app.register(userRoutes, {
+  prefix: '/users',
+})
 
 app.listen({ port: 3100 }, () => {
     console.log('Server is running on port 3100');
