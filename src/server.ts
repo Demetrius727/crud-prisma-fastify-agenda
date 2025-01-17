298298dad7b770b9da05ea2bdef25f87d77cdd23
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-17 19:13:42 -0300

    fastify setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import fastify, { FastifyInstance } from "fastify";
+
+const app: FastifyInstance = fastify();
+
+app.listen({ port: 3100 }, () => {
+    console.log('Server is running on port 3100');
+  });
